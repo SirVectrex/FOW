bcbcb0bc8d780f4294722ca5980cac77622c527c
Florian Schulenberg <florian.schulenberg@web.de>
2021-11-10 17:21:57 +0100

    initial commit

---
+++ ToDoList/weather_access.js
@@ -0,0 +1,11 @@
+async function getData() {
+    const response = await fetch('https://api.publicapis.org/entries')
+
+    const data = await response.json()
+    console.log("test" + response)
+
+
+
+}
+
+getData
